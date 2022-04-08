749c0e93435c8a4dd1d2418bd141ea825a7cab72
Andrés Mata <andres_mata21@outlook.com>
2022-04-08 15:42:12 -0500

    Pruebas de promesas

---
+++ src/tests/base/09-promesas.test.js
@@ -0,0 +1,21 @@
+import { getHeroeByIdAsync } from "../../base/09-promesas";
+import heroes from "../../data/heroes";
+
+describe('Pruebas con promesas', () => {
+    test('debe retornar un héroe async', (done) => {
+        const id = 1;
+        getHeroeByIdAsync(id).then(heroe => {
+            expect(heroe).toBe(heroes[0]);
+            done();
+        });
+    });
+
+    test('debe retornar un error si el héroe por ID no existe', (done) => {
+        const id = 10;
+
+        getHeroeByIdAsync(id).catch(error => {
+            expect(error).toBe('No se pudo encontrar el héroe');
+            done();
+        });
+    });
+});
