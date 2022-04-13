5d9d499fdf902ff2ff0d6e7096663ee9067a18ec
Andrés Mata <andres_mata21@outlook.com>
2022-04-13 14:39:49 -0500

    Pruebas en componente finalizado

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import './index.css';
+// Antes React 18
+/*import './index.css';
 import React from "react";
 import ReactDOM from "react-dom";
 import PrimeraApp from "./PrimeraApp";
@@ -6,4 +7,17 @@ import PrimeraApp from "./PrimeraApp";
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render(<PrimeraApp value="Hola, Soy Goku" />, divRoot);
+ReactDOM.render(<PrimeraApp value="Hola, Soy Goku" />, divRoot);*/
+
+
+// React 18
+import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import PrimeraApp from "./PrimeraApp";
+// import CounterApp from './CounterApp';
+
+const divRoot = document.querySelector('#root');
+const root = createRoot(divRoot);
+
+root.render(<PrimeraApp saludo='Hola, Soy Goku' />);
--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
 const { render } = require("@testing-library/react");
+import '@testing-library/jest-dom/extend-expect'
 import PrimeraApp from "../PrimeraApp";
 
 describe('Pruebas en <PrimeraApp />', () => {
     test('debe de mostrar el mensaje: "Hola, Soy Goku', () => {
         const saludo = 'Hola, Soy Goku';
         // Función que recibe un componente que queramos renderizar
-        const wrapper = render(<PrimeraApp saludo={saludo} />)
+        const { getByText } = render(<PrimeraApp saludo={saludo} />);
+
+        expect(getByText(saludo)).toBeInTheDocument();
     });
 });
