2c6f52f0723620518f8ce751bb17915a9ca74b8c
Andrés Mata <andres_mata21@outlook.com>
2022-04-13 17:07:45 -0500

    Prueba de Enzyme al componente PrimeraApp

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,14 +1,22 @@
-import React from 'react';
-const { render } = require("@testing-library/react");
-import '@testing-library/jest-dom/extend-expect'
+// import React from 'react';
+// const { render } = require("@testing-library/react");
+// import '@testing-library/jest-dom/extend-expect'
 import PrimeraApp from "../PrimeraApp";
+import { shallow } from 'enzyme';
 
 describe('Pruebas en <PrimeraApp />', () => {
-    test('debe de mostrar el mensaje: "Hola, Soy Goku', () => {
-        const saludo = 'Hola, Soy Goku';
-        // Función que recibe un componente que queramos renderizar
-        const { getByText } = render(<PrimeraApp saludo={saludo} />);
+    // test('debe de mostrar el mensaje: "Hola, Soy Goku', () => {
+    //     const saludo = 'Hola, Soy Goku';
+    //     // Función que recibe un componente que queramos renderizar
+    //     const { getByText } = render(<PrimeraApp saludo={saludo} />);
+    //     expect(getByText(saludo)).toBeInTheDocument();
+    // });
 
-        expect(getByText(saludo)).toBeInTheDocument();
+    test('debe de mostrar <PrimeraApp /> correctamente', () => {
+        const saludo = 'Hola, Soy Goku';
+        const wrapper = shallow(<PrimeraApp saludo={saludo} />);
+
+        expect(wrapper).toMatchSnapshot();
     });
+
 });
