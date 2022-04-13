f2397a4d1b719f77e0eeb0d086f98ab63e230009
Andrés Mata <andres_mata21@outlook.com>
2022-04-13 18:36:01 -0500

    Pruebas en componente del Contador

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -2,8 +2,16 @@ import { shallow } from "enzyme";
 import CounterApp from '../CounterApp';
 
 describe('Pruebas en <CounterApp />', () => {
-    test('debe de mostrar los valores por defecto de props', () => {
+    test('debe de mostrar los valores por defecto', () => {
         const wrapper = shallow(<CounterApp />);
         expect(wrapper).toMatchSnapshot();
+    });
+
+    test('debe de mostrar el valor tomando el valor pasado al componente al props', () => {
+        const wrapper = shallow(<CounterApp value={100} />);
+
+        const valorContador = wrapper.find('h2').text();
+
+        expect(valorContador).toBe('100');
     });
 });
